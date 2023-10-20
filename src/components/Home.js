630b11f46b54a4d034aaef7cb0e64b7dcd5b9281
fedabeqain <feda.beqain@gmail.com>
2023-10-20 22:09:32 +0300

    lab completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import * as images from "../data/user";
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {images.username} is a Web Developer from {images.city}
       </h1>
     </div>
   );
 }
+
+export default Home;
